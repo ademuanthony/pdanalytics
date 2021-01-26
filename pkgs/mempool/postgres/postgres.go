aff46240e3f68eb41e9dc21b5a6a7af28a06d9d5
Ademu Anthony <ademuanthony@gmail.com>
2021-01-26 09:50:10 +0100

    Database connection

--- pkgs/mempool/postgres/postgres.go
+++ pkgs/mempool/postgres/postgres.go
@@ -16,7 +16,36 @@ import (
 )
 
 type PgDb struct {
-	db *sql.DB
+	db           *sql.DB
+	queryTimeout time.Duration
+}
+
+type logWriter struct{}
+
+func (l logWriter) Write(p []byte) (n int, err error) {
+	log.Debug(string(p))
+	return len(p), nil
+}
+
+func NewPgDb(host, port, user, pass, dbname string, debug bool) (*PgDb, error) {
+	db, err := Connect(host, port, user, pass, dbname)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(5)
+	if debug {
+		boil.DebugMode = true
+		boil.DebugWriter = logWriter{}
+	}
+	return &PgDb{
+		db:           db,
+		queryTimeout: time.Second * 30,
+	}, nil
+}
+
+func (pg *PgDb) Close() error {
+	log.Trace("Closing postgresql connection")
+	return pg.db.Close()
 }
 
 func (pg PgDb) MempoolTableName() string {
